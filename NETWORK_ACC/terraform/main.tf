a53c94ea6c40e04eeb14e09417fede47334f02fe
wellsiau-aws <wellsiau@amazon.com>
2024-05-26 12:24:37 -0700

    update network

--- NETWORK_ACC/terraform/main.tf
+++ NETWORK_ACC/terraform/main.tf
@@ -31,73 +31,10 @@ module "ipam" {
   }
 }
 
-# ---------- AWS SECRETS MANAGER ----------
-
-# IPAM Pool Secret
-resource "aws_secretsmanager_secret" "ipam_pool" {
-  name                    = "${data.aws_caller_identity.aws_networking_account.account_id}-ipam-pool-id"
-  description             = "IPAM Pool (Networking Account)."
-  kms_key_id              = aws_kms_key.secrets_key.arn
-  recovery_window_in_days = 0
-}
-
-resource "aws_secretsmanager_secret_version" "ipam_pool" {
-  secret_id     = aws_secretsmanager_secret.ipam_pool.id
-  secret_string = module.ipam.pools_level_2["usa/spoke"].id
-}
-
-
-# KMS Key to encrypt the secrets
-resource "aws_kms_key" "secrets_key" {
-  description             = "KMS Secrets Key - Security Account."
-  enable_key_rotation     = true
-}
-
-# KMS Policy
-data "aws_iam_policy_document" "policy_kms_document" {
-  statement {
-    sid    = "Enable AWS Secrets Manager secrets decryption."
-    effect = "Allow"
-    actions = [
-      "kms:Decrypt",
-      "kms:Encrypt",
-      "kms:*"
-    ]
-    resources = ["*"]
-
-    principals {
-      type        = "AWS"
-      identifiers = [data.aws_caller_identity.aws_networking_account.account_id]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "kms:ViaService"
-
-      values = ["secretsmanager.${data.aws_region.current.name}.amazonaws.com"]
-    }
-    condition {
-      test     = "StringLike"
-      variable = "kms:EncryptionContext:SecretARN"
-
-      values = ["arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.aws_networking_account.id}:secret:*"]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "aws:PrincipalOrgID"
-
-      values = ["${data.aws_organizations_organization.org.id}"]
-    }
-  }
-
-  statement {
-    sid       = "Enable IAM User Permissions"
-    actions   = ["kms:*"]
-    resources = ["arn:aws:kms:${data.aws_region.current.name}:${data.aws_caller_identity.aws_networking_account.id}:*"]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.aws_networking_account.id}:root"]
-    }
-  }
+# ---------- SSN PARAMETER ------------
+resource "aws_ssm_parameter" "ipam_pool" {
+  provider = aws.aft_management
+  name  = "aft/network/ipam/pool/spokes/id"
+  type  = "String"
+  value = module.ipam.pools_level_2["usa/spoke"].id
 }
