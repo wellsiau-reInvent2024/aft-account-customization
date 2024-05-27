0b862851c6ae417a12402f3b055850d30ca33c2e
wellsiau-aws <wellsiau@amazon.com>
2024-05-26 17:55:36 -0700

    add spoke acc

--- SPOKE_ACC/terraform/main.tf
+++ SPOKE_ACC/terraform/main.tf
@@ -1,20 +1,16 @@
-# ---------- AWS ORGANIZATIONS AND ACCOUNT INFORMATION ----------
-data "aws_caller_identity" "aws_networking_account" {}
-data "aws_organizations_organization" "org" {}
-data "aws_region" "current" {}
-
-data "aws_ssm_parameter" "spoke_ipam_pool_id" {
+# data ssm parameter 
+data "aws_ssm_parameter" "ipam_pool_id" {
   name = "/aft/account-request/custom-fields/ipam_pool_id"
 }
 
-# VPCs
+# AWS-IA (Integration Automation) VPC module
 module "vpcs" {
   source   = "aws-ia/vpc/aws"
   version  = "4.4.2"
 
-  name                    = "AFT-Vended"
+  name                    = "IPAM Vended VPC"
   az_count                = 3
-  vpc_ipv4_ipam_pool_id   = data.aws_ssm_parameter.spoke_ipam_pool_id.value
+  vpc_ipv4_ipam_pool_id   = data.aws_ssm_parameter.ipam_pool_id.value
   vpc_ipv4_netmask_length = 24
 
   subnets = {
