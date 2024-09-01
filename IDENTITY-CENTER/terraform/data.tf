bf23bbdd0a345ef7d7de014c0cb8115455089321
wellsiau-aws <wellsiau@amazon.com>
2024-09-01 14:28:13 -0700

    sso

---
+++ IDENTITY-CENTER/terraform/data.tf
@@ -0,0 +1,11 @@
+data "aws_iam_policy" "bedrock_full_access_managed_policy" {
+  name = "AmazonBedrockFullAccess"
+}
+
+data "aws_iam_policy" "ec2_readonly_managed_policy" {
+  name = "AmazonEC2ReadOnlyAccess"
+}
+
+data "aws_iam_policy" "poweruser_managed_policy" {
+  name = "PowerUserAccess"
+}
