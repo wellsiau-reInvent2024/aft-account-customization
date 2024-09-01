2b239006ce00e870136de0cd015eac0fca705576
wellsiau-aws <wellsiau@amazon.com>
2024-09-01 16:48:23 -0700

    nonsensitive

--- GENAI-WORKLOAD/terraform/locals.tf
+++ GENAI-WORKLOAD/terraform/locals.tf
@@ -1,13 +1,13 @@
 locals {
   sso = {
     user = {
-      user_name  = unsensitive(data.aws_ssm_parameter.sso_user.value)
-      first_name = unsensitive(data.aws_ssm_parameter.sso_first_name.value)
-      last_name  = unsensitive(data.aws_ssm_parameter.sso_last_name.value)
-      email      = unsensitive(data.aws_ssm_parameter.sso_email.value)
+      user_name  = nonsensitive(data.aws_ssm_parameter.sso_user.value)
+      first_name = nonsensitive(data.aws_ssm_parameter.sso_first_name.value)
+      last_name  = nonsensitive(data.aws_ssm_parameter.sso_last_name.value)
+      email      = nonsensitive(data.aws_ssm_parameter.sso_email.value)
     }
     group = {
-      name = unsensitive(data.aws_ssm_parameter.sso_group.value)
+      name = nonsensitive(data.aws_ssm_parameter.sso_group.value)
     }
   }
 }
