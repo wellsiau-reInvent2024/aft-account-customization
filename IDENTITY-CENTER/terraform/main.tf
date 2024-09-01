a8f827964171213a27c97d639586d5e492b576c8
wellsiau-aws <wellsiau@amazon.com>
2024-09-01 15:27:58 -0700

    for_each_ssm

--- IDENTITY-CENTER/terraform/main.tf
+++ IDENTITY-CENTER/terraform/main.tf
@@ -1,32 +1,6 @@
 // This is a template file for a basic deployment.
 // Modify the parameters below with actual values
 
-# module "aws-iam-identity-center" {
-#   source  = "aws-ia/iam-identity-center/aws"
-#   version = "1.0.0"
-
-#   permission_sets = {
-#     SandboxAccess = {
-#       description      = "AWS power user access permissions.",
-#       session_duration = "PT4H",
-#       aws_managed_policies = [
-#         data.aws_iam_policy.poweruser_managed_policy.arn
-#       ]
-#       tags = { ManagedBy = "AFT" }
-#     },
-
-#     CustomPermissionAccess = {
-#       description      = "Custom permissions for genai workload",
-#       session_duration = "PT4H",
-#       aws_managed_policies = [
-#         data.aws_iam_policy.bedrock_full_access_managed_policy.arn,
-#         data.aws_iam_policy.ec2_readonly_managed_policy.arn
-#       ]
-#       tags = { ManagedBy = "AFT" }
-#     },
-#   }
-# }
-
 module "aws-iam-identity-center" {
   for_each = local.new_permission_sets
   source   = "aws-ia/iam-identity-center/aws"
