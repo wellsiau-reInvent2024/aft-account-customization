52b4161e71863db8e9bb876c87d691e9c260d081
wellsiau-aws <wellsiau@amazon.com>
2024-09-01 17:04:45 -0700

    fix misc

--- GENAI-WORKLOAD/terraform/main.tf
+++ GENAI-WORKLOAD/terraform/main.tf
@@ -16,8 +16,8 @@ module "aws-iam-identity-center" {
     "${local.sso.user.user_name}" : {
       group_membership = [local.sso.group.name]
       user_name        = local.sso.user.user_name
-      given_name       = local.sso.user.given_name
-      family_name      = local.sso.user.family_name
+      given_name       = local.sso.user.first_name
+      family_name      = local.sso.user.last_name
       email            = local.sso.user.email
     },
   }
